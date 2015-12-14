c0647f53ac0d3ec41be0346ec75c6adf3bf42f84
sichevoi <sichevoi@gmail.com>
2015-12-14 15:05:49 +0100

    Fixed score text

--- Assets/Scripts/ScoresManager.cs
+++ Assets/Scripts/ScoresManager.cs
@@ -4,18 +4,17 @@ using System.Collections;
 
 public class ScoresManager : MonoBehaviour {
 
-	public string scoreText = "Score: ";
-
 	private Text _scoreText;
 	private long _score = 0;
 
 	// Use this for initialization
 	void Start () {
 		_scoreText = FindObjectOfType<Text> ();
+		_scoreText.text = "0";
 	}
 
 	public void IncrementScore() {
 		_score += 1;
-		_scoreText.text = scoreText + _score;
+		_scoreText.text = _score.ToString();
 	}	
 }
