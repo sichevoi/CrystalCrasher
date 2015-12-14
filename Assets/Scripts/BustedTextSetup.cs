0a76e271f1e2a7e0a9e27a1cde2664a57c2526df
sichevoi <sichevoi@gmail.com>
2015-12-14 18:01:18 +0100

    Added score to death screen

---
+++ Assets/Scripts/BustedTextSetup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BustedTextSetup : MonoBehaviour {
+
+	public int[] scores = new int[] {10, 50, 100, 1000};
+	public string[] texts = new string[] {
+		"You stole {0} crystals. At least you'll be able to buy cigaretes in prison.",
+		"But you managed to steal {0} crystals. This would be enough to get yourself a luxery cell!",
+		"But you managed to steal {0} crystals. This would be enough to bribe the judge!",
+		"But you managed to steal {0} crystals. This would be enough to bribe the whole state!"};
+
+	// Use this for initialization
+	void Start () {
+		Text bustedText = GetComponent<Text> ();
+		long score = Toolbox.Instance.currentScore;
+
+		for (int i = 0; i < scores.Length; ++i) {
+			if (score < scores[i]) {
+				bustedText.text = string.Format(texts[i], score);
+				break;
+			}
+		}
+	}
+}
